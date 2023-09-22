890659671bf6627ebef02e197d2ff49d4f7ff20d
wjacobw <williamjacob0910@gmail.com>
2023-09-22 20:09:26 +0800

    adds the basic functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,34 @@ import { useEffect } from "react";
 import { useState } from "react";
 import "./App.css";
 
+
+
 function App() {
   const [data, setData] = useState([]);
+  const [random, setRandom] = useState(Math.floor(Math.random() * data.length));
+  const [name, setName] = useState("");
+  const [point, setPoint] = useState(0);
+  const [maxPoint, setMaxPoint] = useState(0);
+
+
+  function checkName() {
+    
+    if (name === data[random]?.name) {
+      setPoint(point + 1)
+      setRandom(Math.floor(Math.floor(Math.random() * data.length)))
+      if (point + 1 > maxPoint) {
+        setMaxPoint(point + 1)
+      }
+    } else {
+      setPoint(0)
+      setRandom(Math.floor(Math.floor(Math.random() * data.length)))
+    }
+  }
+  function handleInput(e) {
+    setName(e.target.value);
+   
+  }
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,12 +52,23 @@ function App() {
     };
     fetchData();
   }, []);
+  
+
+ 
 
   return (
-    <div>
-      {data.map((pokemon) => (
-        <img key={pokemon.name} src={pokemon.img}></img>
-      ))}
+    <div className = "game">
+      <div className = "score">
+      <p>Current Score: {point}</p>
+      <p>High Score: {maxPoint}</p>
+      
+      </div>
+      <img src = {data[random]?.img }></img>
+      <hr/>
+      
+      <input onChange = {handleInput} placeholder = "Pokemon Name"/>
+      &nbsp;&nbsp;&nbsp;
+      <button onClick = {checkName}>Confirm</button>
     </div>
   );
 }
